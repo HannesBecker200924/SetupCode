bb109e485466701288690c94d852251a60bc317d
HannesBecker200924 <hannesbecker07@gmail.com>
2026-06-19 18:18:42 +0200

    Update New_Solver

---
+++ New_Solver/build/CMakeFiles/SPH.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/Benchmarks/benchmarks_single_grain.cu" "CMakeFiles/SPH.dir/src/Benchmarks/benchmarks_single_grain.cu.o" "gcc" "CMakeFiles/SPH.dir/src/Benchmarks/benchmarks_single_grain.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/actions_gpu.cu" "CMakeFiles/SPH.dir/src/actions_gpu.cu.o" "gcc" "CMakeFiles/SPH.dir/src/actions_gpu.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/eigen_solver.cu" "CMakeFiles/SPH.dir/src/eigen_solver.cu.o" "gcc" "CMakeFiles/SPH.dir/src/eigen_solver.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/grid_gpu_base.cu" "CMakeFiles/SPH.dir/src/grid_gpu_base.cu.o" "gcc" "CMakeFiles/SPH.dir/src/grid_gpu_base.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/grid_gpu_green.cu" "CMakeFiles/SPH.dir/src/grid_gpu_green.cu.o" "gcc" "CMakeFiles/SPH.dir/src/grid_gpu_green.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/grid_gpu_rothlin.cu" "CMakeFiles/SPH.dir/src/grid_gpu_rothlin.cu.o" "gcc" "CMakeFiles/SPH.dir/src/grid_gpu_rothlin.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/interactions_gpu.cu" "CMakeFiles/SPH.dir/src/interactions_gpu.cu.o" "gcc" "CMakeFiles/SPH.dir/src/interactions_gpu.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/leap_frog.cu" "CMakeFiles/SPH.dir/src/leap_frog.cu.o" "gcc" "CMakeFiles/SPH.dir/src/leap_frog.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/particle_gpu.cu" "CMakeFiles/SPH.dir/src/particle_gpu.cu.o" "gcc" "CMakeFiles/SPH.dir/src/particle_gpu.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/tool_3d_gpu.cu" "CMakeFiles/SPH.dir/src/tool_3d_gpu.cu.o" "gcc" "CMakeFiles/SPH.dir/src/tool_3d_gpu.cu.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/Benchmarks/benchmarks_test.cpp" "CMakeFiles/SPH.dir/src/Benchmarks/benchmarks_test.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/Benchmarks/benchmarks_test.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/Benchmarks/interp_utils.cpp" "CMakeFiles/SPH.dir/src/Benchmarks/interp_utils.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/Benchmarks/interp_utils.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/Benchmarks/solid_state_processes.cpp" "CMakeFiles/SPH.dir/src/Benchmarks/solid_state_processes.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/Benchmarks/solid_state_processes.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/blanking.cpp" "CMakeFiles/SPH.dir/src/blanking.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/blanking.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/constants_structs.cpp" "CMakeFiles/SPH.dir/src/constants_structs.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/constants_structs.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/geometry_utils.cpp" "CMakeFiles/SPH.dir/src/geometry_utils.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/geometry_utils.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/grid_cpu_tri.cpp" "CMakeFiles/SPH.dir/src/grid_cpu_tri.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/grid_cpu_tri.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/json_reader.cpp" "CMakeFiles/SPH.dir/src/json_reader.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/json_reader.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/ldynak_reader.cpp" "CMakeFiles/SPH.dir/src/ldynak_reader.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/ldynak_reader.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/mfree_gpu_3d.cpp" "CMakeFiles/SPH.dir/src/mfree_gpu_3d.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/mfree_gpu_3d.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/surface_tri_mesh.cpp" "CMakeFiles/SPH.dir/src/surface_tri_mesh.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/surface_tri_mesh.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/tool_3d.cpp" "CMakeFiles/SPH.dir/src/tool_3d.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/tool_3d.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/types.cpp" "CMakeFiles/SPH.dir/src/types.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/types.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/vtk_reader.cpp" "CMakeFiles/SPH.dir/src/vtk_reader.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/vtk_reader.cpp.o.d"
+  "/home/1beckerh@fzg.local/FS-SPH-GPU-ESAFORM-new/src/vtk_writer.cpp" "CMakeFiles/SPH.dir/src/vtk_writer.cpp.o" "gcc" "CMakeFiles/SPH.dir/src/vtk_writer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
